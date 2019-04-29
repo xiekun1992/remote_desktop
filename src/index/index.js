73d0048e23b61b1fb9d5874856c8ce4e25b0d52b
xiekun1992 <xkfront@outlook.com>
2019-04-29 23:52:26 +0800

    修改信令服务器地址

--- src/index/index.js
+++ src/index/index.js
@@ -4,7 +4,7 @@ const SignalConnection = require('../signal_connection');
 const RTC = require('../rtc');
 
 // const connection = new SignalConnection().connect('192.168.3.31', 8080);
-const connection = new SignalConnection().connect('192.168.1.101', 8080);
+const connection = new SignalConnection().connect('13.231.201.110', 8080);
 let signalConn, serialNum, rtcConnection, targetUser;
 Promise
     .all([connection, si.diskLayout()])
